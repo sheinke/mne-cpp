d6033b0b57ad80eeaf308fed16b6bb3e875745e1
Simon Heinke <simon.heinke@tu-ilmenau.de>
2018-04-05 21:46:45 +0200

    [MA-7] Minor style change to ExtensionManager and fixed CopyRight

--- applications/mne_analyze/libs/anShared/Management/extensionmanager.cpp
+++ applications/mne_analyze/libs/anShared/Management/extensionmanager.cpp
@@ -2,13 +2,15 @@
 /**
 * @file     extensionmanager.cpp
 * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Lars Debor <lars.debor@tu-ilmenau.de>;
+*           Simon Heinke <simon.heinke@tu-ilmenau.de>;
 *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 * @version  1.0
 * @date     February, 2017
 *
 * @section  LICENSE
 *
-* Copyright (C) 2017, Christoph Dinh and Matti Hamalainen. All rights reserved.
+Copyright (C) 2017, Christoph Dinh, Lars Debor, Simon Heinke and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
@@ -75,8 +77,9 @@ ExtensionManager::ExtensionManager(QObject *parent)
 
 ExtensionManager::~ExtensionManager()
 {
-    for(int i = 0; i < m_qVecExtensions.size(); ++i) {
-        delete m_qVecExtensions[i];
+    foreach(IExtension* extension, m_qVecExtensions)
+    {
+        delete extension;
     }
 }
 
