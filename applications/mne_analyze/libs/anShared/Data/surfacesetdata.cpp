2092942bcea92472874b3d4bba149ba603044785
1DIce <lars.debor@tu-ilmenau.de>
2018-04-30 14:12:21 +0200

    [MA-6] Updated the docu of SurfaceSetData.

--- applications/mne_analyze/libs/anShared/Data/surfacesetdata.cpp
+++ applications/mne_analyze/libs/anShared/Data/surfacesetdata.cpp
@@ -2,13 +2,14 @@
 /**
 * @file     surfacesetdata.cpp
 * @author   Lars Debor <lars.debor@tu-ilmenau.de>;
+*           Simon Heinke <simon.heinke@tu-ilmenau.de>;
 *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 * @version  1.0
 * @date     March, 2018
 *
 * @section  LICENSE
 *
-* Copyright (C) 2018, Lars Debor and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2018, Lars Debor, Simon Heinke and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
@@ -78,6 +79,9 @@ using namespace FSLIB;
 SurfaceSetData::SurfaceSetData()
 {
 }
+
+
+//*************************************************************************************************************
 
 SurfaceSetData::SurfaceSetData(const QString &subject_id, qint32 hemi, const QString &surf, const QString &subjects_dir)
 {
