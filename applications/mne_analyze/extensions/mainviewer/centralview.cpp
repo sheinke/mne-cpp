426b6bd5a5ece7a972f47a7bb1cdb22ddff49ae6
sheinke <simon.heinke@tu-ilmenau.de>
2018-08-24 18:25:47 +0200

    [MA-8] Changed CentralView's render policy to On-Demand-Rendering.

--- applications/mne_analyze/extensions/mainviewer/centralview.cpp
+++ applications/mne_analyze/extensions/mainviewer/centralview.cpp
@@ -119,6 +119,9 @@ void CentralView::init()
     m_vEntities.push_back(QSharedPointer<QEntity>(pCamController));
 
     setRootEntity(m_pRootEntity);
+
+    //Only render new frames when needed
+    this->renderSettings()->setRenderPolicy(Qt3DRender::QRenderSettings::OnDemand);
 }
 
 
